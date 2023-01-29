ae1955cfa79f48d826f03a58ca6609315c385751
CameronWDavis <77825507+CameronWDavis@users.noreply.github.com>
2023-01-28 22:18:05 -0500

    Add files via upload

---
+++ mechanics.py
@@ -0,0 +1,10 @@
+class Moves:
+
+    def __init__(self, name, type,category, contest, PP, power,accuracy):
+        self.name = name
+        self.type = type
+        self.category = category
+        self.contest = contest
+        self.PP = PP
+        self.power = power
+        self.accuracy = accuracy
