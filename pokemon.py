05aaf55c51334a9a9699541aa831fe88ff9ff97a
CameronWDavis <77825507+CameronWDavis@users.noreply.github.com>
2023-01-29 22:47:34 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,3 +1,5 @@
+from Color import colors #import for color
+
 class newPokemon:
     def __init__(self,name,type,health,attack,defense,height,weight,moves):
         self.name = name
@@ -8,4 +10,23 @@ class newPokemon:
         self.weight = weight
         self.height = height
         self.moves = moves
+
+    def getType(self):
+        color = ""
+        match self.type:
+            case "Electric":
+                color = colors.fontcolor.yellow
+                return color
+            case "Fire":
+                color = colors.fontcolor.red
+                return color
+            case "Water":
+                color = colors.fontcolor.blue
+                return color
+            case "Grass":
+                color = colors.fontcolor.green
+                return color
+            case "Normal":
+                color = colors.fontcolor.lightgrey
+                return color
 
