7a0faadd4a67370cd13d151086799793d36d80c4
CameronWDavis <77825507+CameronWDavis@users.noreply.github.com>
2023-02-04 21:58:52 -0500

    Update Color.py

--- Color.py
+++ Color.py
@@ -1,18 +1,13 @@
+#class for colors
+#author Cameron Davis
+#purpose of this  is to add colors to the terminla to make things look nice
 class colors:
-    reset = '\033[0m'
+    reset = '\033[0m' #reset the color
     class fontcolor:
-        black = '\033[30m'
         red = '\033[31m'
         green = '\033[32m'
-        orange = '\033[33m'
         blue = '\033[34m'
         purple = '\033[35m'
-        cyan = '\033[36m'
         lightgrey = '\033[37m'
-        darkgrey = '\033[90m'
-        lightred = '\033[91m'
-        lightgreen = '\033[92m'
         yellow = '\033[93m'
-        lightblue = '\033[94m'
         pink = '\033[95m'
-        lightcyan = '\033[96m'
