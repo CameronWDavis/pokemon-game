167d23687e1effe966d5d9ac56187e8573eace1e
CameronWDavis <77825507+CameronWDavis@users.noreply.github.com>
2023-02-04 21:52:00 -0500

    Update mechanics.py

--- mechanics.py
+++ mechanics.py
@@ -1,6 +1,8 @@
 from Color import colors #import for color
+#author Cameron Davis
+#this is a class to generate a move object
 class Moves:
-
+#this is a constructor for the move
     def __init__(self, name, type,category, contest, PP, power,accuracy):
         self.name = name
         self.type = type
@@ -9,10 +11,8 @@ class Moves:
         self.PP = PP
         self.power = power
         self.accuracy = accuracy
-        
-        
-#this is a function for color and changing the color of console codes
 
+ #this gets the type of a move to send back the color
     def getType(self):
         color = ""
         match self.type:
@@ -33,4 +33,4 @@ class Moves:
                 return color
             case _:
                  color = colors.fontcolor.lightgrey
-                 return co
+                 return color
